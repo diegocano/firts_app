cd0dd9d7151f8e6267b54218ea03863249618512
diegocano <cpucano@gmail.com>
2013-07-03 20:50:37 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirtsApp::Application.config.secret_key_base = '57cc3b4f1de7ff56161d217d2f50ecef04e115b2393d71f324a870351e3197a5d3dfca4122cb25992e0c365b7befb8e7d62eeba343daa22113dde25374b8887d'
